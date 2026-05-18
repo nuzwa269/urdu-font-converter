cb2d0421346f010f6032d1bf2ce381751e7d5914
nuzwa269 <blsoha424@gmail.com>
2026-05-18 13:54:26 +0500

    Delete public directory

--- public/bookmarklet-loader.js
+++
@@ -1,6 +0,0 @@
-/* Loader injected by the bookmarklet. Fetches the main script. */
-(function(){
-  var s = document.createElement("script");
-  s.src = (window.__NASTALEEQ_BASE__ || "") + "/nastaleeq.js?v=" + Date.now();
-  document.body.appendChild(s);
-})();
